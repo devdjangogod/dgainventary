143ff956828cb6f48b95a7349422350182cf9881
joel <jjcomecar@unitru.edu.pe>
2026-06-11 00:10:17 -0500

    Migracion a PostgreSQL y actualizacion de modelos

--- systemdga/settings.py
+++ systemdga/settings.py
@@ -59,12 +59,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'systemdga.wsgi.application'
 
+
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
+
 
 LANGUAGE_CODE = 'es-pe'
 TIME_ZONE = 'America/Lima'
